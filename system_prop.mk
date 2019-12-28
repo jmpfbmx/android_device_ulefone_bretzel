214287f064ee395565110f9c3fda0a688df56821
Kirill <vsegda_online_007@bk.ru>
2019-12-28 13:29:14 +0300

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -5,3 +5,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
+    
+# Telephony
+ro.telephony.default_network=9,9
