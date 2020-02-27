fbf6b5809416edfc950b1aad5b5cc3113db4e482
Kirill <vsegda_online_007@bk.ru>
2020-02-27 14:00:04 +0300

    Create vendor_props.mk

---
+++ vendor_props.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2019 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.release_type=release \
+    ro.vendor.build.real.device=true \
+    vendor.product.device=A5_Pro \
+    vendor.product.manufacturer=UMIDIGI
