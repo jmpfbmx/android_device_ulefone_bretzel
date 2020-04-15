6bec3f90f5c804a0af9128af36f90f1e377547b3
Kirill <vsegda_online_007@bk.ru>
2020-04-15 18:38:50 +0300

    Switch to Pixel 4 XL (flame) build fingerprint

--- xtended_A5_Pro.mk
+++ xtended_A5_Pro.mk
@@ -41,7 +41,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="A5_Pro-user 9 PPR1.180610.011 1559618636 release-keys"
 
 # Fingerprint
-BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys
+BUILD_FINGERPRINT := "google/flame/flame:10/QQ2A.200405.005/6254899:user/release-keys"
 
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
