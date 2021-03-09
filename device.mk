6224443591dbc258374a30ac2288df93041d1f1b
LuK1337 <priv.luk@gmail.com>
2021-03-09 10:48:15 +0100

    bretzel: Build libhidltransport/libhwbinder

--- device.mk
+++ device.mk
@@ -37,7 +37,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Display
 PRODUCT_PACKAGES += \
